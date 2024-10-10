2a957d667fd070bbf41b604d2ff1ad2cebfd0a19
Paul Manis <pmanis@med.unc.edu>
2024-10-09 23:29:18 -0400

    partly working w/ pyqt

--- src/sound_search.py
+++ src/sound_search.py
@@ -2,17 +2,19 @@ import pyvisa as pv
 import time
 import numpy as np
 
-# import tkinter as TK
-# from tkinter import ttk
 import threading
 from threading import Event
-import sounddevice as SD
-import sound
+
 import pyqtgraph as pg
 from pyqtgraph.parametertree import Parameter, ParameterTree
 import pyqtgraph.dockarea as PGD
+from pyqtgraph.Qt.QtCore import QObject, QRunnable, QThreadPool, pyqtSlot, pyqtSignal
+from pyqtgraph.Qt import QtCore, QtWidgets
+import sounddevice as SD
+import sound
 
-DEFAULT_AUDIO_RATE = 44100
+DEFAULT_AUDIO_RATE = 44100  # 96000  # 44100  # in Hz. Mac Mini will do 96K (see via Utility)
+THREAD_PERIOD = 20  # thread period, in msec
 
 
 def attach_sdg():
@@ -20,7 +22,7 @@ def attach_sdg():
     resources = rm.list_resources()
     resnum = None
     for i, res in enumerate(resources):
-        print("resource: ", res)
+        # print("resource: ", res)
         if res.find("SDG0") >= 0:
             resnum = i
     if resnum is None:
@@ -37,38 +39,214 @@ def play_wave(wave, rate):
     # play a waveform on the default sound device.
     # Wave is the waveform
     # rate is the sample rate at which the waveform was generated.
-
     # downsample wave for speaker
-    twave = np.linspace(0, len(wave) / rate, len(wave))
-    tmax = np.max(twave)
-    tnew = np.arange(0, tmax, 1.0 / rate)
-    dwave = np.interp(tnew, twave, wave)
+    # twave = np.linspace(0, len(wave) / rate, len(wave))
+    # tmax = np.max(twave)
+    # tnew = np.arange(0, tmax, 1.0 / rate)
+    # dwave = np.interp(tnew, twave, wave)
     # now play it.
-    SD.play(dwave, rate)
+
+    SD.play(wave, rate)
+    SD.wait()  # duration of sound is set by the waveform
+    time.sleep(0.01)
+    SD.stop()
+
+
+class WorkerSignals(QRunnable, QObject):
+    """
+    Defines the signals available from a running worker thread.
+
+    Supported signals are:
+
+        finished
+            No data returned
+        error
+            returns tuple (exctype, value, traceback.format_exc() )
+        result
+            returns object data returned from processing, anything
+        paused
+            no data returned
+        resume
+            no data returned
+
+    """
+
+    finished = QtCore.pyqtSignal()
+    error = QtCore.pyqtSignal(tuple)
+    result = QtCore.pyqtSignal(object)
+    paused = QtCore.pyqtSignal()
+    resume = QtCore.pyqtSignal()
+    start = QtCore.pyqtSignal()
+
+    # def __init__(cls):
+    #     super(WorkerSignals, cls).__init__()
+    #     pass
+    # def run(cls):
+    #     pass
+
+
+# threaded class to handle parameter changes while keeping gui open
+# includes ability to pause and resume.
+class Worker(QObject):
+
+    sig_finished = QtCore.pyqtSignal()
+    sig_error = QtCore.pyqtSignal(tuple)
+    sig_result = QtCore.pyqtSignal(object)
+    sig_paused = QtCore.pyqtSignal()
+    sig_resume = QtCore.pyqtSignal()
+    sig_start = QtCore.pyqtSignal()
+
+    def __init__(self, parameters=None):
+        """
+        Params
+        ------
+        UI_params: UI class to read settings and report values
+
+        """
+        super(Worker, self).__init__()
+        self._paused = False  # flag for suspencing thread
+        self._running = False  # to stop the event
+        self.frequency = 1000.0
+        self.old_freq = self.frequency
+        self.duration = 0.05
+        self.old_dur = self.duration
+        self.interval = 0.2
+        self.old_interval = self.interval
+        self.wave = None
+        # self.signals = WorkerSignals()
+        self.params = parameters
+        print("worker init complete...")
+
+    def run(self):
+        pass
+
+    @pyqtSlot()
+    def run_stim(self):
+        """
+        Check the parameters periodically (often enough to seem responsive)
+        present the stimuli as needed.
+        """
+        print("worker run")
+        while self._running:
+            if self._paused:
+                self.signals.paused.emit()
+                break
+            if self.params.device == "SDG810":
+                self.params.sdg810.write("C1:OUTP ON")
+                self.params.sdg810.query("*OPC?")
+                time.sleep(self.duration)
+                self.params.sdg810.write("C1:OUTP OFF")
+            else:
+                # play a sound on the soundcard
+                # print("dur: ", new_dur, "freq; ", new_freq)
+                # only recompute the waveform if uupdate is needed
+                if (
+                    (self.frequency != self.old_freq)
+                    or self.wave is None
+                    or (self.duration != self.old_dur)
+                ):
+                    self.wave = sound.TonePip(
+                        rate=DEFAULT_AUDIO_RATE,
+                        f0=self.frequency,
+                        duration=self.duration,
+                        dbspl=None,
+                        pip_duration=self.duration,
+                        pip_starts=[0.0],
+                        ramp_duration=0.005,
+                    )
+
+                play_wave(self.wave.sound, DEFAULT_AUDIO_RATE)
+
+            time.sleep(self.interval - float(THREAD_PERIOD / 1000.0))
+
+            time.sleep(float(THREAD_PERIOD / 1000.0))  # Short delay to prevent excessive CPU usage
+        print("running ended")
+
+    @pyqtSlot(float)
+    def set_frequency(self, freq: float):
+        print("slot freq")
+        self.frequency = freq
+
+    @pyqtSlot(float)
+    def set_duration(self, duration: float):
+        print("slot dur")
+        self.duration = duration
+        print("... duration: ", duration)
+
+    @pyqtSlot(float)
+    def set_interval(self, interval:float):
+        print("slot Interval")
+        self.interval = interval
+
+    # @pyqtSlot()
+    # def start(self):
+    #     print("start called")
+    #     if not self._running:
+    #         self._running = True
+    # self.run()
+    # self.run()
+
+    @pyqtSlot()
+    def pause(self):
+        # pause the thread - during some update operations, and
+        # when transmitting.
+        print("slot pause")
+        self._paused = True  # set False to block the thread
+        self._running = False
+        self.sig_paused.emit()
+
+    @pyqtSlot()
+    def start(self):
+        """
+        Start the thread.
+        """
+        print("slot start")
+        if not self._running:
+            print("setting running")
+            self._running = True
+            self._paused = False
+            self.run_stim()
+            self.sig_start.emit()
+
+    @pyqtSlot()
+    def stop(self):
+        """
+        Stop the thread from running.
+        This is needed at the end of the program to terminate cleanly.
+        """
+        self._paused = False  # resume if paused
+        self._running = False  # set running False
+        self.sig_finished.emit()  # send a signal
 
 
 # sdg810 = attach_sdg()
-class Stim:
+class AudioStimulator(QObject):
+
+    # set up some signals
+    signal_change_frequency = QtCore.pyqtSignal(float)
+    signal_change_duration = QtCore.pyqtSignal(float)
+    signal_change_interval = QtCore.pyqtSignal(float)
+    signal_paused = QtCore.pyqtSignal()
+    signal_start = QtCore.pyqtSignal()
+    signal_stop = QtCore.pyqtSignal()
+
     def __init__(self):
         # first find the hardware:
-        self.sdg810 = attach_sdg()
-        if self.sdg810 is None:
-            # use soundcard
-            pass
+        self.sdg810 = attach_sdg()  # result will be None if no sdg180 found, then use soundcard
         self.event = Event()
-        # self.root = TK.Tk()
-        # self.root.title("SDG810 Controller: pulsing")
-        print("sdg 810 found? ", self.sdg810)
-        self.running = False
-        self.thread = None
-        self.stop_thread = False
-        self.frequency = 1000.
-        self.duration = 0.05
-        self.interval = 0.2
         if self.sdg810 is not None:
             self.device = "SDG810"
         else:
             self.device = "Soundcard"
+        print(f"Device is {self.device:s}")
+        self.running = False
+        self.thread = None
+        self.stop_thread = False
+        self.frequency = 1000.0
+        self.duration = 0.05
+        self.interval = 0.2
+        super(AudioStimulator, self).__init__()
+
         ptreewidth = 200
         self.app = pg.mkQApp()
         self.params = [
@@ -135,20 +313,59 @@ class Stim:
         self.Dock_Params.addWidget(self.ptree)
         self.win.setCentralWidget(self.dockArea)
 
-        self.running = True
         self.win.show()
         self.ptreedata.sigTreeStateChanged.connect(self.command_dispatcher)
-        # self.root.mainloop()
+        self.threadpool = QThreadPool()  # threadpool will be instantiated in the start routine
+
+        self.timer = pg.Qt.QtCore.QTimer()
+        self.timer.setInterval(20)
+        self.timer.timeout.connect(self.recurring_timer)
+        self.timer.start()
+        print("Gui set up")
+
+        self.Stimulation = Worker(parameters=self)
+        # print(dir(self.Stimulation))
+        print("stimulation worker created")
+        self.signal_change_frequency.connect(self.Stimulation.set_frequency)
+        print("... 1")
+        self.signal_change_duration.connect(self.Stimulation.set_duration)
+        # print("... 2")
+        self.signal_change_interval.connect(self.Stimulation.set_interval)
+        print("... 3")
+        self.signal_paused.connect(self.Stimulation.pause)
+        print("... 4")
+        self.signal_start.connect(self.Stimulation.start)
+        print("... 5")
+        self.Stimulation.sig_finished.connect(self.done)
+        self.signal_stop.connect(self.Stimulation.stop)
+        # self.signal_paused.emit()
+        print("... 6")
+
+        print("signals created")
+        # self.Stimulation.signals.finished.connect(self.done)
+        self.threadpool.start(self.Stimulation.start)  # start reading the updated parameters
+
+        print("thread started")
+
+    def recurring_timer(self):
+        # print("recurring")
+        pass
+
+    def getdur(self):
+        return self.duration
+    
+    def done(self):
+        pass
 
     def command_dispatcher(self, param, changes):
-        # print("param: ", param)
+        print("param: ", param)
         print("changes: ", changes)
         for param, change, data in changes:
             path = self.ptreedata.childPath(param)
-            # print("path: ", path)
+            print("path: ", path)
             match path[0]:
                 case "Device":
-                    if data == 'SDG810' and self.sdg810 is not None:
+                    if data == "SDG810" and self.sdg810 is not None:
                         self.device = data
                     else:
                         self.device = "Soundcard"
@@ -158,104 +375,48 @@ class Stim:
                     self.start()
                 case "Stop":
                     self.stop()
-                case ("Stimulus", "Frequency"):
-                    self.frequency = float(data)
-                    self.event.set()
-                case ("Stimulus", "Duration"):
-                    self.duration = float(data)
-                    self.event.set()
-                case ("Stimulus", "Interval"):
-                    self.interval = float(data)
+                case "Stimulus":
+                    match path[1]:
+                        case "Frequency":
+                            self.signal_change_frequency.emit(float(data))
+                        case "Duration":
+                            print("dur hit", float(data))
+                            self.signal_change_duration.emit(float(data))
+                        case "Interval":
+                            self.signal_change_interval.emit(float(data))
                 case _:
                     pass
 
-    def showfreq(self, event):
-        self.frequency = float(event)
-        return
-
     def start(self):
-        if self.thread is None:  # only start once.
+        print("***** start called")
+        self.signal_start.emit()
 
-            self.thread = threading.Thread(target=self.run, args=(self.duration, self.frequency))
-            self.thread.start()
-            self.running = True
-        else:
-            self.running = True  # set the running flag.
-    
-    def get_duration(self):
-        return self.duration
-    
-    def get_freq(self):
-        return self.frequency
-    
-    def run(self, duration, frequency):
-        time.sleep(0.1)
-        # print("Called run: ", sdg810.query("*IDN?"))
-        if self.sdg810 is not None:
-            self.sdg810.write("*RST")
-        while True:
-            print("self.event: ", self.event.is_set())
-            if self.event.is_set():
-                duration = self.duration
-                frequency = self.frequency
-                print("duration: ", duration)
-                self.event.clear()
-            if self.running:
-                if self.device == "SDG810":
-                    self.sdg810.write("C1:OUTP ON")
-                    self.sdg810.query("*OPC?")
-                    time.sleep(duration)
-                else:
-                    # play a sound on the soundcard
-                    print("sound card output: ")
-                    print("dur: ", duration, "freq; ", frequency)
-                    wave = sound.TonePip(
-                        rate=DEFAULT_AUDIO_RATE,
-                        f0=frequency,
-                        duration=duration,
-                        dbspl=None,
-                        pip_duration=duration,
-                        pip_starts=[0.0],
-                        ramp_duration=0.005,
-                    )
-                    play_wave(wave.sound, DEFAULT_AUDIO_RATE)
-                    SD.wait()  # duration of sound is set by the waveform
+    def stop(self):
+        print("***** stop called")
+        self.signal_stop.emit()
+        # self.Stimulation.stop()
 
-                # print(self.sdg810.query("C1:OUTP?"))
-                if self.device == "SDG810":
-                    self.sdg810.write("C1:OUTP OFF")
-                else:
-                    SD.stop()
-                time.sleep(self.interval)
-                # print("...", self.sdg810.query("C1:OUTP?"))
-            elif self.stop_thread:
-                if self.sdg810 is not None:
-                    self.sdg810.write("C1:OUTP OFF")
-                else:
-                    SD.stop()
-                break
-            else:
-                time.sleep(0.02)
+    # def get_duration(self, verbose: bool = False):
+    #     return self.duration
+
+    # def get_frequency(self, verbose: bool = False):
+    #     return self.frequency
 
     def quit(self):
         print("Quitting")
-        self.running = False
-        self.stop_thread = True
+        self.stop()
         if self.sdg810 is not None:
             self.sdg810.write("C1:OUTP OFF")
         else:
-            SD.wait()
+            # SD.wait()
             SD.stop()
-        if self.thread is not None:
-            self.thread.join(2)
-        # self.root.destroy()
+        self.threadpool.waitForDone(5 * THREAD_PERIOD)
+        # if self.thread is not None:
+        #     self.thread.join(2)
         exit()
-
-    def stop(self):
-        self.running = False
 
 
 if __name__ == "__main__":
 
-    STIM = Stim()
+    STIM = AudioStimulator()
     pg.exec()
